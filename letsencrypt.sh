afc01324b79aae23d0e0cfe2ec59df5d856aef75
dabico <dabico@usi.ch>
2023-12-19 00:20:29 +0100

    Update letsencrypt.sh

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -2,6 +2,9 @@
 
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
+mkdir -r ssl/seart-dl4se.si.usi.ch
+mkdir -r ssl/seart-ghs.si.usi.ch
+
 cp /etc/letsencrypt/live/seart-dl4se.si.usi.ch/cert.pem \
     ssl/seart-dl4se.si.usi.ch/cert.pem
 cp /etc/letsencrypt/live/seart-dl4se.si.usi.ch/privkey.pem \
